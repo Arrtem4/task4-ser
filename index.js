d8f403c46a477e727728f1cf54f5e011cf93ce5c
Artem Yakovenko <yakovenko.art1@gmail.com>
2023-08-20 13:07:08 +0500

    h

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(
         name: "session",
         secret: process.env.SECRET_KEY,
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        sameSite: 'none',
         // secure: true,
     })
 );
